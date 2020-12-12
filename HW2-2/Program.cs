6206c6d1f00b6861d246919461a24b17797e3633
Uryupin <urdmitriy@yandex.ru>
2020-12-12 14:14:27 +0300

    Выполнение ДЗ2

---
+++ HW2-2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер текущего месяца");
+            int mounthN = Convert.ToInt32(Console.ReadLine());
+            string mounthW = null;
+            switch (mounthN)
+            {
+                case 1:
+                    mounthW = "Январь";
+                    break;
+                case 2:
+                    mounthW = "Февраль";
+                    break;
+                case 3:
+                    mounthW = "Март";
+                    break;
+                case 4:
+                    mounthW = "Апрель";
+                    break;
+                case 5:
+                    mounthW = "Май";
+                    break;
+                case 6:
+                    mounthW = "Июнь";
+                    break;
+                case 7:
+                    mounthW = "Июль";
+                    break;
+                case 8:
+                    mounthW = "Август";
+                    break;
+                case 9:
+                    mounthW = "Сентябрь";
+                    break;
+                case 10:
+                    mounthW = "Октябрь";
+                    break;
+                case 11:
+                    mounthW = "Ноябрь";
+                    break;
+                case 12:
+                    mounthW = "Декабрь";
+                    break;
+                default:
+                    mounthW = "Введено неверное значение";
+                    break;
+            }
+            Console.WriteLine($"Вы ввели месяц: {mounthW}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW2-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("Вы ввели четное число");
+            else
+                Console.WriteLine("Вы ввели нечетное число");
+            Console.ReadLine();
+        }
+    }
+}
