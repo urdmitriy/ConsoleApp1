f9a43c3b7bad996eb34551ee6f516f15ce16e9bb
Uryupin <urdmitriy@yandex.ru>
2020-12-25 23:26:59 +0300

    Домашнее задание 6

---
+++ HW6/Proc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace HW6
+{
+    class Proc
+    {
+        public Process[] process { get; private set; }
+
+        public void FillProcessList()
+        {
+            process = Process.GetProcesses();
+        }
+        public void PrintProccList()
+        {
+            for (int i = 0; i < process.Length; i++)
+            {
+                Console.WriteLine($"[{process[i].Id}]\t{process[i].ProcessName}");
+            }
+        }
+
+        public bool KillProcess(int id)
+        {
+            try
+            {
+                Process.GetProcessById(id).Kill();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удалось удалить указанный процесс");
+                return false;
+            }
+            Console.WriteLine("Процесс завершен успешно");
+            return true;
+        }
+    }
+}
---
+++ HW6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proc MyProcess = new Proc();
+            MyProcess.FillProcessList();
+            MyProcess.PrintProccList();
+
+            bool killResult = false;
+
+            while (!killResult)
+            {
+                int idKillProc = 0;
+                Console.WriteLine("Какой процесс (ID) желаете грохнуть?");
+
+                try
+                {
+                    idKillProc = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не удалось получить ID процесса");
+                }
+                
+                killResult = MyProcess.KillProcess(idKillProc);
+                if (!killResult) Console.WriteLine("Еще разок");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
