40f101a8e8f1d7152e116d815089b093766aa40f
Uryupin <urdmitriy@yandex.ru>
2020-12-31 11:31:07 +0300

    Домашнее задание 8

---
+++ HW8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings.Default.greeting);
+            if (string.IsNullOrEmpty(Properties.Settings.Default.userName))
+            {
+                Console.WriteLine("Введите ваше имя");
+                Properties.Settings.Default.userName = Console.ReadLine();
+                Console.WriteLine("Введите ваш возраст");
+                Properties.Settings.Default.age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Чем занимаетесь?");
+                Properties.Settings.Default.work = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine($"Вас зовут {Properties.Settings.Default.userName}, " +
+                    $"вам {Properties.Settings.Default.age} лет, " +
+                    $"вы занимаетесь {Properties.Settings.Default.work}");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Напоминалка")]
+[assembly: AssemblyDescription("Запустить программу 31 декабря, потом 1 января")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HomeMade3000")]
+[assembly: AssemblyProduct("Напоминалка")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fd7b301e-c6e0-4050-8cae-2d190602e810")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
